a1e80b4d19b6003c5a6e9621ca3a0590288b3ab5
Irasov <srnan@yandex.ru>
2025-11-05 21:22:30 +0500

    fix: class Component

--- scripts/utils/component.js
+++ scripts/utils/component.js
@@ -1,26 +1,20 @@
-/*
- *optioons: {
- * tag: 'div' // default 'div'
- * text: 'some text' // default ''
- * classes: [] // default []}
- */
 class Component {
-  children = [];
-  node = null;
-  constructor(options, ...children) {
-    const node = document.createElement(options.tag || 'div');
-    node.textContent = options.text || '';
-    if (options.classes.length > 0) {
-      node.classList.add(...options.classes);
+  #children = [];
+  #node = null;
+  constructor({ tag = 'div', classes = [], text = '' }, ...children) {
+    const node = document.createElement(tag);
+    node.textContent = text;
+    if (classes.length > 0) {
+      node.classList.add(...classes);
     }
-    this.node = node;
+    this.#node = node;
     if (children) {
       this.appendChilren(children);
     }
   }
   append(child) {
-    this.children.push(child);
-    this.node.append(child.getNode());
+    this.#children.push(child);
+    this.#node.append(child.getNode());
   }
   appendChilren(children) {
     children.forEach((child) => {
@@ -28,42 +22,42 @@ class Component {
     });
   }
   getChildren() {
-    return this.children;
+    return this.#children;
   }
   getNode() {
-    return this.node;
+    return this.#node;
   }
   setTextContent(text) {
-    this.node.textContent = text;
+    this.#node.textContent = text;
   }
   setAttribute(attribute, value) {
-    this.node.setAttribute(attribute, value);
+    this.#node.setAttribute(attribute, value);
   }
   removeAttribute(attribute) {
-    this.node.removeAttribute(attribute);
+    this.#node.removeAttribute(attribute);
   }
 
   toggleClass(className) {
-    this.node.classList.toggle(className);
+    this.#node.classList.toggle(className);
   }
 
   addListner(event, listner) {
-    this.node.addEventListener(event, listner);
+    this.#node.addEventListener(event, listner);
   }
 
   removeListner(event, listner) {
-    this.node.removeEventListener(event, listner);
+    this.#node.removeEventListener(event, listner);
   }
 
   destroyChildren() {
-    this.children.forEach((child) => {
+    this.#children.forEach((child) => {
       child.destroy();
     });
-    this.children.length = 0;
+    this.#children.length = 0;
   }
 
   destroy() {
-    if (this.children.length > 0) this.destroyChildren();
-    this.node.remove();
+    if (this.#children.length > 0) this.destroyChildren();
+    this.#node.remove();
   }
 }
